f507b676ecf4748fd6bad26073d0a00fbe9544d3
NicolaChiesa <chiesanicola54@gmail.com>
2023-05-24 23:27:50 +0200

    directory updated

--- 01_LinearRegression/linearRegression.py
+++ 01_LinearRegression/linearRegression.py
@@ -2,34 +2,42 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
+
 plt.rcParams['figure.figsize'] = (10.0, 7.0)
 
-# Preprocessing Input data
-data = pd.read_csv('headBrain.csv')
-X = data.iloc[:, 2]
-Y = data.iloc[:, 3]
-plt.scatter(X, Y)
-plt.xlabel('Age')
-plt.ylabel('Head Circonference')
-plt.show()
+# # Preprocessing Input data
+current_directory = os.getcwd()
+dataFolder = os.path.join(current_directory,"Data")
+file_path = os.path.join(dataFolder, "headBrain.csv")
+data = pd.read_csv(file_path)
+# X = data.iloc[:, 2]
+# Y = data.iloc[:, 3]
+# plt.scatter(X, Y)
+# plt.xlabel('Age')
+# plt.ylabel('Head Circonference')
+# plt.show()
 
-# Building the model
-X_mean = np.mean(X)
-Y_mean = np.mean(Y)
+# # Building the model
+# X_mean = np.mean(X)
+# Y_mean = np.mean(Y)
 
-num = 0
-den = 0
-for i in range(len(X)):
-    num += (X[i] - X_mean)*(Y[i] - Y_mean)
-    den += (X[i] - X_mean)**2
-m = num / den
-c = Y_mean - m*X_mean
+# num = 0
+# den = 0
+# for i in range(len(X)):
+#     num += (X[i] - X_mean)*(Y[i] - Y_mean)
+#     den += (X[i] - X_mean)**2
+# m = num / den
+# c = Y_mean - m*X_mean
 
-print (m, c)
+# print (m, c)
 
-# Making predictions
-Y_pred = m*X + c
+# # Making predictions
+# Y_pred = m*X + c
 
-plt.scatter(X, Y) # actual
-plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color='red') # predicted
-plt.show()
+# plt.scatter(X, Y) # actual
+# plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color='red') # predicted
+# plt.show()
+
+# print('fatto')
--- 01_LinearRegression/tempCodeRunnerFile.py
+++ 01_LinearRegression/tempCodeRunnerFile.py
@@ -1,1 +1,30 @@
-data
+data = pd.read_csv('headBrain.csv')
+# X = data.iloc[:, 2]
+# Y = data.iloc[:, 3]
+# plt.scatter(X, Y)
+# plt.xlabel('Age')
+# plt.ylabel('Head Circonference')
+# plt.show()
+
+# # Building the model
+# X_mean = np.mean(X)
+# Y_mean = np.mean(Y)
+
+# num = 0
+# den = 0
+# for i in range(len(X)):
+#     num += (X[i] - X_mean)*(Y[i] - Y_mean)
+#     den += (X[i] - X_mean)**2
+# m = num / den
+# c = Y_mean - m*X_mean
+
+# print (m, c)
+
+# # Making predictions
+# Y_pred = m*X + c
+
+# plt.scatter(X, Y) # actual
+# plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color='red') # predicted
+# plt.show()
+
+# print('fatto')
